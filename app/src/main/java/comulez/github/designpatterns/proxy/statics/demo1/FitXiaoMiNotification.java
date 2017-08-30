a9f6a9155e83d3fb75414b032f5ab4765bd8e637
吴国平 <guoping_wu@suishouji.com>
2017-08-30 09:27:02 +0800

    增加静态代理的一个应用场景

---
+++ app/src/main/java/comulez/github/designpatterns/proxy/statics/demo1/FitXiaoMiNotification.java
@@ -0,0 +1,21 @@
+package comulez.github.designpatterns.proxy.statics.demo1;
+
+/**
+ * <pre>
+ *     @author guoping_wu
+ *     @since 2017/08/30 09:22
+ *     @version v1.0
+ *     Desc
+ * </pre>
+ */
+public class FitXiaoMiNotification implements INotification {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
---
+++ app/src/main/java/comulez/github/designpatterns/proxy/statics/demo1/INotification.java
@@ -0,0 +1,14 @@
+package comulez.github.designpatterns.proxy.statics.demo1;
+
+/**
+ * <pre>
+ *     @author guoping_wu
+ *     @since 2017/08/30 09:20
+ *     @version v1.0
+ *     Desc
+ * </pre>
+ */
+public interface INotification {
+    void show();
+    void hide();
+}
